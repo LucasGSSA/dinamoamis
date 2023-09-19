d0395d60e594d11c5363984a7c0a70e76f8e04fc
LukanONE <lucasgabriel.silveira10@gmail.com>
2023-09-18 23:49:39 -0300

    coisa

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 let cartasNoticias = [];
-class noticias = {
-    this.name
+let id = 0;
+class noticia {
+    constructor(nome, titulo, data, resumo){
+        this.name = nome;
+        this.titulo = titulo;
+        this.data = data;
+        this.resumo = resumo;
+        this.id = ++id;
+    }
 }
+
+
+
+
+
+// const amis = new noticia("jogo sta","Amis 5 Sta 0", "19/09/2023","amis ganhou de lavada n tem como");
+// const amis2 = new noticia("jogo sta","Amis 5 Sta 0", "19/09/2023","amis ganhou de lavada n tem como");
+// const ami3 = new noticia("jogo sta","Amis 5 Sta 0", "19/09/2023","amis ganhou de lavada n tem como");
+// cartasNoticias.push(amis);
+// cartasNoticias.push(amis2);
+// cartasNoticias.push(ami3);
+
+// console.log(cartasNoticias);
+// console.log(id);
